8e7214aaca378b4f0f39b01335276b37662956cb
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2023-12-14 04:57:12 -0600

    Solution for part 1 of Day 12 problem

---
+++ day12/README.md
@@ -0,0 +1,4 @@
+Execute this code via `dune exec ./day7.exe <filename>`.
+
+### Observations
+
---
+++ day12/day12.ml
@@ -0,0 +1,130 @@
+type record = { spring_spans : char list list; damaged_springs : int list }
+
+let ( >> ) f g x = g (f x)
+
+let parse filename transform1 transform2 =
+  let parse_line line =
+    let tmp1 = String.split_on_char ' ' line in
+    let spring_spans =
+      List.nth tmp1 0 |> transform1 |> String.split_on_char '.'
+      |> List.filter (fun x -> x <> String.empty)
+      |> List.map (String.to_seq >> List.of_seq)
+    in
+    let damaged_springs =
+      List.nth tmp1 1 |> transform2 |> String.split_on_char ','
+      |> List.map (String.trim >> Int32.of_string >> Int32.to_int)
+    in
+    { spring_spans; damaged_springs }
+  in
+  let lines = In_channel.with_open_text filename In_channel.input_lines in
+  lines |> List.map parse_line
+
+let process records =
+  (* let stringify a =
+       a |> List.map (List.to_seq >> String.of_seq) |> String.concat ","
+     in
+  let open Printf in *)
+  let rec get_count record =
+    (* printf "(%s) (%s) \n"
+       (stringify record.spring_spans)
+       (record.damaged_springs |> List.map Int.to_string |> String.concat ","); *)
+    let ret =
+      match record with
+      | { spring_spans = s; damaged_springs = [] } ->
+          if s |> List.for_all (fun x -> List.for_all (fun x -> x <> '#') x)
+          then 1
+          else 0
+      | { spring_spans = []; damaged_springs = _ :: _ } -> 0
+      | { spring_spans = [] :: ss_tl; damaged_springs = ds } ->
+          get_count { spring_spans = ss_tl; damaged_springs = ds }
+      | { spring_spans = [ '#' ] :: ss_tl; damaged_springs = 1 :: ds_tl } ->
+          get_count { spring_spans = ss_tl; damaged_springs = ds_tl }
+      | { spring_spans = [ '?' ] :: ss_tl; damaged_springs = 1 :: ds_tl } ->
+          get_count { spring_spans = ss_tl; damaged_springs = ds_tl }
+          + get_count { spring_spans = ss_tl; damaged_springs = 1 :: ds_tl }
+      | {
+       spring_spans = ('#' :: e :: ss_hd_tl) :: ss_tl;
+       damaged_springs = 1 :: ds_tl;
+      } ->
+          if e <> '#' then
+            get_count
+              { spring_spans = ss_hd_tl :: ss_tl; damaged_springs = ds_tl }
+          else 0
+      | {
+       spring_spans = ('?' :: e :: ss_hd_tl) :: ss_tl;
+       damaged_springs = 1 :: ds_tl;
+      } ->
+          let x =
+            if e <> '#' then
+              get_count
+                { spring_spans = ss_hd_tl :: ss_tl; damaged_springs = ds_tl }
+            else 0
+          in
+          let y =
+            get_count
+              {
+                spring_spans = (e :: ss_hd_tl) :: ss_tl;
+                damaged_springs = 1 :: ds_tl;
+              }
+          in
+          x + y
+      | { spring_spans = ss_hd :: ss_tl; damaged_springs = n :: ds_tl }
+        when n = List.length ss_hd
+             && List.for_all (fun x -> x = '#' || x = '?') ss_hd ->
+          let x = get_count { spring_spans = ss_tl; damaged_springs = ds_tl } in
+          x
+          +
+          if List.for_all (fun x -> x = '?') ss_hd then
+            get_count
+              {
+                spring_spans = List.tl ss_hd :: ss_tl;
+                damaged_springs = n :: ds_tl;
+              }
+          else 0
+      | { spring_spans = ss_hd :: _; damaged_springs = n :: _ }
+        when n > List.length ss_hd && List.exists (fun x -> x = '#') ss_hd ->
+          0
+      | { spring_spans = ss_hd :: ss_tl; damaged_springs = n :: ds_tl }
+        when n > List.length ss_hd && List.for_all (fun x -> x = '?') ss_hd ->
+          get_count { spring_spans = ss_tl; damaged_springs = n :: ds_tl }
+      | { spring_spans = ss_hd :: ss_tl; damaged_springs = n :: ds_tl }
+        when n < List.length ss_hd ->
+          let x =
+            if
+              ss_hd
+              |> List.filteri (fun i _ -> i < n)
+              |> List.for_all (fun x -> x = '#' || x = '?')
+              && List.nth ss_hd n <> '#'
+            then
+              get_count
+                {
+                  spring_spans =
+                    (ss_hd |> List.filteri (fun i _ -> i > n)) :: ss_tl;
+                  damaged_springs = ds_tl;
+                }
+            else 0
+          in
+          let y =
+            if List.hd ss_hd <> '#' then
+              get_count
+                {
+                  spring_spans = List.tl ss_hd :: ss_tl;
+                  damaged_springs = n :: ds_tl;
+                }
+            else 0
+          in
+          x + y
+      | _ -> failwith "Invalid state"
+    in
+    ret
+  in
+  records |> List.map get_count |> List.fold_left ( + ) 0
+
+let () =
+  let filename = Sys.argv.(1) in
+  let open Printf in
+  let id x = x in
+  let transform1 x = String.concat "?" (Array.make 5 x |> Array.to_list) in
+  let transform2 x = String.concat "," (Array.make 5 x |> Array.to_list) in
+  printf "%d\n" (process (parse filename id id));
+  printf "%d\n" (process (parse filename transform1 transform2))
