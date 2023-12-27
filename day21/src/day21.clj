93c90b285939a6f64feb3a5b2711b8d1e0a50941
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2023-12-26 19:20:13 -0600

    A part of the solution to part 2 of the 21st day problem

--- day21/src/day21.clj
+++ day21/src/day21.clj
@@ -1,5 +1,6 @@
 (ns day21
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :refer [union]]))
 
 (defn get-matching-positions [rows num-rows num-cols match?]
   (let [matching-positions (for [row (range 0 num-rows)
@@ -42,13 +43,36 @@
                            (map (partial get neighbors))
                            (apply concat)
                            (set)))
-        last-positions (reduce take-a-step (hash-set start-pos) 
+        last-positions (reduce take-a-step (set [start-pos])
                                (range num-steps))]
-    (println (count last-positions))))  
+    (println (count last-positions))))
 
-(defn -main [filename]
+;; partial solution to part2
+(defn solve-part2 [neighbors num-rows num-cols start-pos num-steps]
+  (let [workset (hash-set start-pos)
+        take-a-step (fn [[positions odd-set even-set] n]
+                      (let [n-is-even (= (rem n 2) 0)
+                            next-set (if n-is-even even-set odd-set)
+                            next-positions (->> positions
+                                                (map (partial get neighbors))
+                                                (apply concat)
+                                                (remove #(contains? next-set %1))
+                                                (set))
+                            [odd-set even-set] (if n-is-even
+                                                 [odd-set (union even-set next-positions)]
+                                                 [(union odd-set next-positions) even-set])]
+                        [next-positions odd-set even-set]))
+        [positions odd-set even-set] (reduce take-a-step
+                                             [(set [start-pos]) (hash-set) (hash-set)]
+                                             (range num-steps))
+        last-positions (if (= (rem num-steps 2) 0) odd-set even-set)]
+    (println (count last-positions))))
+
+(defn -main [filename num-steps]
   (with-open [file (clojure.java.io/reader filename)]
     (let [lines (remove str/blank? (line-seq file))
           [num-rows num-cols neighbors] (identify-neighbors lines)
-          start-pos (find-start-position lines num-rows num-cols)]
-      (solve-part1 neighbors num-rows num-cols start-pos 10))))
+          start-pos (find-start-position lines num-rows num-cols)
+          num-steps (Integer/parseInt num-steps)]
+      (solve-part1 neighbors num-rows num-cols start-pos num-steps)
+      (solve-part2 neighbors num-rows num-cols start-pos num-steps))))
