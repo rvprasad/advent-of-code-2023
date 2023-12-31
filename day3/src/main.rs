6542db583327c0f18b411a0e6e212bcd6bb7c682
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2023-12-30 18:19:04 -0600

    Clean up

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use partial_application::partial;
 use std::collections::HashMap;
 use std::collections::HashSet;
@@ -9,24 +8,25 @@ use std::io::BufReader;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let content = get_content(&args[1]);
-    let symbol_and_affected_positions = get_symbol_affected_positions(&content).unwrap();
-    let part_num_and_positions = get_part_num_to_positions(&content).unwrap();
-    process1(&symbol_and_affected_positions, &part_num_and_positions);
-    process2(&symbol_and_affected_positions, &part_num_and_positions);
+    if let Ok(content) = get_content(&args[1]) {
+        let symbol_and_affected_positions = get_symbol_affected_positions(&content);
+        let part_num_and_positions = get_part_num_to_positions(&content);
+        process1(&symbol_and_affected_positions, &part_num_and_positions);
+        process2(&symbol_and_affected_positions, &part_num_and_positions);
+    }
 }
 
-fn get_content(filename: &String) -> Vec<Vec<char>> {
-    let f = File::open(filename).unwrap();
+fn get_content(filename: &String) -> std::io::Result<Vec<Vec<char>>> {
+    let f = File::open(filename)?;
     let reader = BufReader::new(f);
-    let lines = reader.lines().map(|l| l.unwrap()).filter_map(|l| {
+    let lines = reader.lines().flatten().filter_map(|l| {
         if l.is_empty() {
             None
         } else {
             Some(l.chars().collect())
         }
     });
-    return Vec::from_iter(lines);
+    Ok(Vec::from_iter(lines))
 }
 
 fn process2(
@@ -97,9 +97,7 @@ fn process1(
     println!("{}", relevant_part_nums.iter().fold(0, |acc, x| acc + x));
 }
 
-fn get_symbol_affected_positions(
-    content: &Vec<Vec<char>>,
-) -> Result<Vec<(char, HashSet<(i32, i32)>)>> {
+fn get_symbol_affected_positions(content: &Vec<Vec<char>>) -> Vec<(char, HashSet<(i32, i32)>)> {
     let mut ret = Vec::new();
     let non_symbols = Vec::from_iter("0123456789.".chars());
     for (row_num, row) in content.iter().enumerate() {
@@ -108,8 +106,8 @@ fn get_symbol_affected_positions(
                 continue;
             }
             let mut positions = HashSet::new();
-            let row_i32 = i32::try_from(row_num)?;
-            let col_i32 = i32::try_from(col_num)?;
+            let row_i32 = row_num as i32;
+            let col_i32 = col_num as i32;
             for r in -1..=1 {
                 for c in -1..=1 {
                     positions.insert((row_i32 + r, col_i32 + c));
@@ -118,18 +116,18 @@ fn get_symbol_affected_positions(
             ret.push((*elem, positions));
         }
     }
-    return Ok(ret);
+    ret
 }
 
-fn get_part_num_to_positions(content: &Vec<Vec<char>>) -> Result<Vec<(i32, HashSet<(i32, i32)>)>> {
+fn get_part_num_to_positions(content: &Vec<Vec<char>>) -> Vec<(i32, HashSet<(i32, i32)>)> {
     let mut ret = Vec::new();
     let digits = Vec::from_iter("0123456789".chars());
     for (row_num, row) in content.iter().enumerate() {
-        let row_i32 = i32::try_from(row_num)?;
+        let row_i32 = row_num as i32;
         let mut captured_digits = Vec::new();
         let last_col = row.len() - 1;
         for (col_num, elem) in row.iter().enumerate() {
-            let mut col_i32 = i32::try_from(col_num)?;
+            let mut col_i32 = col_num as i32;
             let process_captured_digits;
             if digits.contains(elem) {
                 captured_digits.push(*elem);
@@ -140,13 +138,16 @@ fn get_part_num_to_positions(content: &Vec<Vec<char>>) -> Result<Vec<(i32, HashS
             }
             if process_captured_digits {
                 let num_positions = HashSet::from_iter(
-                    (0..i32::try_from(captured_digits.len())?).map(|i| (row_i32, col_i32 - i)),
+                    (0..(captured_digits.len() as i32)).map(|i| (row_i32, col_i32 - i)),
                 );
-                let number = i32::from_str_radix(&String::from_iter(captured_digits.clone()), 10)?;
-                ret.push((number, num_positions));
-                captured_digits.clear();
+                if let Ok(number) =
+                    i32::from_str_radix(&String::from_iter(captured_digits.clone()), 10)
+                {
+                    ret.push((number, num_positions));
+                    captured_digits.clear();
+                }
             }
         }
     }
-    return Ok(ret);
+    ret
 }
